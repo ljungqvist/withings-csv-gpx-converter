f761bcf226cec7fc2f79cf67d7e9fdb4367eea91
Petter Ljungqvist <petter@ljungqvist.info>
2024-10-24 08:12:42 +0300

    updated dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,8 +8,8 @@
 
 plugins {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
-    id("org.jetbrains.kotlin.jvm") version "1.3.72"
-    kotlin("kapt") version "1.4.20"
+    id("org.jetbrains.kotlin.jvm") version "2.0.21"
+    kotlin("kapt") version "2.0.21"
 
     // Apply the application plugin to add support for building a CLI application in Java.
     application
@@ -17,7 +17,7 @@ plugins {
 
 repositories {
     // Use JCenter for resolving dependencies.
-    jcenter()
+    mavenCentral()
 }
 
 dependencies {
@@ -28,12 +28,12 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
     // This dependency is used by the application.
-    implementation("com.google.guava:guava:29.0-jre")
+    implementation("com.google.guava:guava:33.3.1-jre")
 
-    implementation("com.opencsv:opencsv:3.8")
+    implementation("com.opencsv:opencsv:5.9")
 
-    implementation("com.squareup.moshi:moshi-kotlin:1.11.0")
-    kapt("com.squareup.moshi:moshi-kotlin-codegen:1.11.0")
+    implementation("com.squareup.moshi:moshi-kotlin:1.15.1")
+    kapt("com.squareup.moshi:moshi-kotlin-codegen:1.15.1")
 
     // Use the Kotlin test library.
     testImplementation("org.jetbrains.kotlin:kotlin-test")
